fa69febab38e5cbebe681cac562861f3f37428e2
Riccardo Rossetti <rossetti.riccardo@outlook.com>
2023-09-30 11:00:53 -0400

    update gaussian amp

--- gaussian_matrix.R
+++ gaussian_matrix.R
@@ -1,68 +1,99 @@
-### Pure Gaussian noise: is the unadjusted recursion accurately tracked by a Gaussian?
-## Define some reasonable denoisers
-
-# Soft-thresholding
-ST = function(y,theta) {
-  if (y > theta) 
-    return(y - theta)
-  else if (y <= -theta)
-    return(theta + y)
-  else
-    return(0)
-}
-
-ST_deriv = function(y, theta) {
-  if (y > theta) 
-    return(1)
-  else if (y <= -theta)
-    return(-1)
-  else
-    return(0)
-}
-
-# Tanh nonlinearity
-TH = function(y, theta) {
-  tanh(sqrt(theta) * y)
-}
-
-## Make noise matrices
-# Sample Gaussian matrix
-rgaus_mat = function(n, spherical = TRUE) {
-  matrix(rnorm(n^2, sd = n^(spherical * -1/2)), n, n)
-}
-
-# Sample GOE
-rgoe = function( n, scale = TRUE ) {
-  out = matrix(0, n, n)
-  vec = rnorm( (n+1)*n / 2, sd = n^(scale * -1/2))
-  out[lower.tri(out, diag = TRUE)] = vec
-  out = out + t(out)
-  return(out)
-}
+### AMP for pure Gaussian or GOE matrices
+source('./helpers.R')
 
 ## AMP recursions
+## Matrix-version implementations
 # Fully Gaussian matrix
-amp_gaus = function(rmat, x, denoise, theta, tol=1e-4) {
-  if (ncol(rmat) != length(init)) {
-    print('Non conformable d8imensions!')
-    return(FALSE)
+amp_gaus = function(x, denoise, theta, spike = NULL, tol = 1e-4) {
+  # Add spike if present
+  if (is.null(spike)) {
+    n = length(x)
+    data = rgaus_mat(n)
+  } else {
+    n = length(x)
+    data = spike + rgaus_mat(n)
   }
   
-  n = length(init)
-  improve = tol + 1e2
+  improve = Inf
   t = 0
   maxt = 100
   iter = matrix(NA, n, 0)
   
-  while ((improve > tol) & (t < maxt)) {
-    t = t+1
+  while( (improve > tol) & (t < maxt) ) {
+    t = t + 1
     f = sapply(x, denoise, theta = theta)
-    x = as.vector(rmat %*% f)
+    x = as.vector(data %*% f)
     iter = cbind(iter, x)
     if (t > 1) {
-      improve = sum(abs( iter[,ncol(iter)] - iter[,(ncol(iter) - 1)] ))
+      improve = abs( var(iter[,ncol(iter)]) - var(iter[,(ncol(iter) - 1)]) )
     }
   }
   
   return(iter)
 }
+
+# GOE matrix
+amp_goe = function(x, denoise, theta, spike = NULL, tol = 1e-4) {
+  # Add spike if present
+  if (is.null(spike)) {
+    n = length(x)
+    data = rgoe(n)
+  } else {
+    n = length(x)
+    data = spike + rgoe(n)
+  }
+  
+  deriv = Deriv(denoise, formalArgs(denoise)[1])
+  
+  improve = Inf
+  t = 0
+  maxt = 100
+  iter = matrix(NA, n, 0)
+  onsager = 0
+  
+  while ( (improve > tol) & (t < maxt) ) {
+    t = t+1
+    f = sapply(x, denoise, theta = theta)
+    x = as.vector(data %*% f) - onsager
+    onsager = sum( sapply(x, deriv, theta = theta) ) / n * f
+    iter = cbind(iter, x)
+    if (t > 1) {
+      improve = abs( var(iter[,ncol(iter)]) - var(iter[,(ncol(iter) - 1)]) )
+    }
+  }
+  
+  return(iter)
+}
+
+## Diagnostics of interest
+# Deviations from normality of the iterates
+deviations = function(iterates) {
+  evalpoints = ppoints(nrow(iterates))
+  quants = qnorm(evalpoints)
+  norm_iters = apply(iterates, 2, function(x) sort(x) / sd(x) )
+  
+  dev = apply(norm_iters, 2, function(x,q) x - q, q = quants)
+  return(dev)
+}
+
+# Normalized overlaps
+overlap = function(x, truth) {
+  abs(cor(x,truth))
+}
+
+## TESTING: COMMENT OUT WHEN DONE
+# Parameter settings
+n = 1000
+signal = rspike(n)
+snr = 2
+spike = sqrt(snr) * signal[[2]]
+init = 0.1 * signal[[1]] + rnorm(n)
+
+# Pure Gaussian noise 
+gaus_iter = amp_gaus(init, dtanh, 1)
+goe_fact = amp_goe(init, dtanh, 1)
+
+# Gaussian signal + noise matrix factorization
+gaus_fact = amp_gaus(init, dtanh, 1, spike = spike)
+goe_fact = amp_goe(init, dtanh, 1, spike = spike)
+
--- helpers.R
+++ helpers.R
@@ -1,6 +1,13 @@
 ### HELPER FUNCTIONS ###
+library(Deriv)
 
-## Sample GOE
+## Make noise matrices
+# Sample Gaussian matrix
+rgaus_mat = function(n, spherical = TRUE) {
+  matrix(rnorm(n^2, sd = n^(spherical * -1/2)), n, n)
+}
+
+# Sample GOE
 rgoe = function( n, scale = TRUE ) {
   out = matrix(0, n, n)
   vec = rnorm( (n+1)*n / 2, sd = n^(scale * -1/2))
@@ -8,3 +15,59 @@ rgoe = function( n, scale = TRUE ) {
   out = out + t(out)
   return(out)
 }
+
+## Some spike distributions (renormalized for second moment)
+# Bernoulli spike
+bspike = function( n, p, scale = TRUE) {
+  v = rbinom(n, 1, p) / sqrt(p)
+  if (scale) spike = tcrossprod(v) / n else spike = tcrossprod(v) / (sqrt(n))
+  return(list(v, spike))
+}
+
+# Rademacher spike
+rspike = function( n, scale = TRUE ) {
+  v = sample(c(-1,1), n, TRUE, c(0.5,0.5))
+  if (scale) spike = tcrossprod(v) / n else spike = tcrossprod(v) / (sqrt(n))
+  return(list(v, spike))
+}
+
+# Sparse Rademacher spike
+srspike = function( n, p, scale = TRUE) {
+  v = sample(c(-1,0,1), n, TRUE, c(p/2, 1-p, p/2)) / sqrt(p)
+  if (scale) spike = tcrossprod(v) / n else spike = tcrossprod(v) / (sqrt(n))
+  return(list(v, spike))
+}
+
+# Gaussian spike
+gspike = function(n, scale = TRUE) {
+  v = rnorm(n)
+  if (scale) spike = tcrossprod(v) / n else spike = tcrossprod(v) / (sqrt(n))
+  return(list(v, spike))
+}
+
+# Bernoulli-Gaussian spike
+bgspike = function(n, p, scale = TRUE) {
+  v = rbinom(n, 1, p) * rnorm(n, sd = 1/sqrt(p))
+  if (scale) spike = tcrossprod(v) / n else spike = tcrossprod(v) / (sqrt(n))
+  return(list(v, spike))
+}
+
+## Some denoiser funcions
+# Soft-thresholding
+dst = function(x, theta) {
+    if (x > theta) {
+      x - theta 
+    } else if (x <= -theta) {
+      x + theta
+    } else {
+      0
+    }
+}
+
+# Tanh (optimal for Rademacher)
+dtanh = function(x, theta) {
+  tanh( sqrt(theta) * x )
+}
+
+# Geometric-memory ReLU
+

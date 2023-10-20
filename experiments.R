2348520cfdfbee7bbed773bca47326cc71a8d0e4
Riccardo Rossetti <rossetti.riccardo@outlook.com>
2023-10-19 22:26:59 -0400

    fix experiment scaling

--- experiments.R
+++ experiments.R
@@ -8,6 +8,7 @@ rm(sources)
 n = 2000
 snr = 2
 signal = rbinom(n, 1, c(0.5,.5))
+scalefactor = norm(signal, '2')^2
 init = rep(1,n)
 
 # Run AMP
@@ -17,8 +18,8 @@ relu_amp_goe = amp_goe(init, relu_denoiser, relu_denoiser_deriv, signal, snr, ma
 # Compute SE quantities
 relu_ftf_iid = crossprod(relu_amp_iid$est) / n
 relu_ftf_goe = crossprod(relu_amp_goe$est) / n
-relu_snr_iid = crossprod(relu_amp_iid$est, signal) / n * snr
-relu_snr_goe = crossprod(relu_amp_goe$est, signal) / n * snr
+relu_snr_iid = crossprod(relu_amp_iid$est, signal) * snr / scalefactor
+relu_snr_goe = crossprod(relu_amp_goe$est, signal) * snr / scalefactor
 
 # Demeaned iterates
 relu_x_center_iid = relu_amp_iid$iter - tcrossprod(signal, relu_snr_iid)
@@ -31,6 +32,7 @@ relu_xtx_goe = cov2cor(crossprod(relu_x_center_goe) / n)
 n = 2000
 snr = 3
 signal = rbinom(n, 1, c(0.5,.5))
+scalefactor = norm(signal, '2')^2
 init = rep(1,n)
 
 # Run AMP
@@ -40,8 +42,8 @@ mrelu_amp_goe = amp_goe(init, memory_relu, memory_relu_deriv, signal, snr, maxt 
 # Compute SE quantities
 mmrelu_ftf_iid = crossprod(mrelu_amp_iid$est) / n
 mrelu_ftf_goe = crossprod(mrelu_amp_goe$est) / n
-mrelu_snr_iid = crossprod(mrelu_amp_iid$est, signal) / n * snr
-mrelu_snr_goe = crossprod(mrelu_amp_goe$est, signal) / n * snr
+mrelu_snr_iid = crossprod(mrelu_amp_iid$est, signal) snr / scalefactor
+mrelu_snr_goe = crossprod(mrelu_amp_goe$est, signal) snr / scalefactor
 
 # Demeaned iterates
 mrelu_x_center_iid = mrelu_amp_iid$iter - tcrossprod(signal, mrelu_snr_iid)

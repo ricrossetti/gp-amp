ed1fbf6be038b2ba4c67e24a4c316aa93f4f7d8a
Riccardo Rossetti <rossetti.riccardo@outlook.com>
2023-10-24 23:49:30 -0400

    update plotting

--- experiments.R
+++ experiments.R
@@ -5,11 +5,11 @@ rm(sources)
 
 ## Positively biased spike, ReLU denoising
 # Parameter settings
-n = 2000
-snr = 1.5
-signal = rbinom(n, 1, c(.5,.5))
+n = 3000
+snr = 1.2
+signal = rbinom(n, 1, c(.1,.9))
 scalefactor = norm(signal, '2')^2
-init = rep(1,n)
+init = rnorm(n, .1)
 
 # Run AMP
 relu_amp_iid = amp_gaus(init, relu_denoiser, signal, snr, maxt = 20)
@@ -23,7 +23,7 @@ relu_snr_goe = crossprod(relu_amp_goe$est, signal) * snr / scalefactor
 
 # Demeaned iterates
 relu_x_center_iid = relu_amp_iid$iter - tcrossprod(signal, relu_snr_iid)
-relu_x_center_goe = relu_amp_iid$iter - tcrossprod(signal, relu_snr_goe)
+relu_x_center_goe = relu_amp_goe$iter - tcrossprod(signal, relu_snr_goe)
 relu_xtx_iid = cov2cor(crossprod(relu_x_center_iid) / n)
 relu_xtx_goe = cov2cor(crossprod(relu_x_center_goe) / n)
 
--- src/plot.R
+++ src/plot.R
@@ -1,4 +1,5 @@
 ### Plotting functions
+library(latex2exp)
 # Heatmap with annotated diagonals
 heat_diag = function(m, diags,...) {
 
@@ -19,4 +20,43 @@ heat_diag = function(m, diags,...) {
   labs = signif(diags, 1)
   text(seq_len(l), rev(seq_len(l)), labels = labs, col = "black", cex = .8)
   
+}
+
+# Pairs plot for assessing normality and SE
+panel.SE <- function(x, y, digits = 2, prefix = "", cex.cor, ...)
+{
+  usr <- par("usr"); on.exit(par("usr" = usr))
+  par(usr = c(0, 1, 0, 1))
+  r <- abs(as.numeric(crossprod(x,y))/(norm(x, '2')*norm(y, '2')))
+  txt <- format(r, digits = digits, nsmall = digits)
+  txt <- paste0(prefix, txt)
+  if(missing(cex.cor)) cex.cor <- 0.5/strwidth(txt)
+  text(0.5, 0.5, txt, cex = cex.cor)
+}
+
+panel.scatter = function(x, y, digits = 2, prefix = "cor = ", ...) {
+  points(x, y)
+  r = cor(x, y)
+  txt = format(r, digits = digits, nsmall = digits)
+  txt = paste0(prefix, txt)
+  coords = par("usr")
+  text( coords[2]-0.05*(coords[2]-coords[1]), coords[3]+0.05*(coords[4]-coords[3]), txt, adj = c(1,0))
+}
+
+panel.hist = function(x, ...) {
+  usr = par("usr"); on.exit(par("usr" = usr))
+  rho = sd(x)
+  h = hist(x, plot = FALSE)
+  breaks = h$breaks; nB = length(breaks)
+  y = h$density
+  par(usr = c(breaks[1], breaks[length(breaks)], 0, 1.5 * max(y)))
+  rect(breaks[-nB], 0, breaks[-1], y, ...)
+  curve(dnorm(x, 0, rho), breaks[1], breaks[length(breaks)], add = TRUE)
+}
+
+pairs_gaus_diagnostics = function(data, subset.spacing = 4) {
+  n = ncol(data)
+  s = seq(1, n, subset.spacing)
+  data.sub = data[,s]
+  pairs(data.sub, labels = paste0('x',s), lower.panel = panel.scatter, upper.panel = NULL, diag.panel = panel.hist, xaxt = 'n', yaxt = 'n')
 }
